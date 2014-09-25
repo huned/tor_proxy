046f99ec48bbcbc1137885ff20c5589209206127
Huned Botee <huned@734m.com>
2014-09-25 11:20:56 -0700

    Replace Tor::Proxy instance methods with a method_missing that delegates to the underlying Net::HTTP.SOCKSProxy instance.

--- lib/tor_proxy.rb
+++ lib/tor_proxy.rb
@@ -5,24 +5,18 @@ require 'tor'
 
 module Tor
   class Proxy
-    def initialize host = 'localhost', port = 9050
+    def initialize(host = 'localhost', port = 9050)
       unless Tor.available?
         raise <<-EOS
           Tor isn't installed. Install Tor to use this module.
           See http://torproject.org or `brew install tor`.
         EOS
       end
-
       @proxy = Net::HTTP.SOCKSProxy host, port
     end
 
-    def get url
-      @proxy.get URI.parse(url)
-    end
-
-    def self.get url
-      @@proxy ||= self.new
-      @@proxy.get url
+    def method_missing(m, *args, &block)
+      @proxy.send(m, *args, &block)
     end
   end
 end
